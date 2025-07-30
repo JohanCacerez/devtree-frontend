612c4e1339d3cd29f3ec99a999f77765ccfceca9
JohanCacerez <johancacerez10@gmail.com>
2025-07-29 20:37:38 -0600

    añadiendo rutas en React router

---
+++ src/views/RegisterView.tsx
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom'
+
+export default function RegisterView() {
+  return (
+    <>
+        <div>RegisterView</div>
+
+        <nav>
+            <Link to="/auth/login">Iniciar sesion</Link>
+        </nav>
+    </>
+  )
+}
