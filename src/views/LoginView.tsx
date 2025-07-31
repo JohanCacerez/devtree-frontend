8e8e074f9e4b86d87d6409154f85ae4e0d02b3d0
JohanCacerez <johancacerez10@gmail.com>
2025-07-30 18:58:28 -0600

    añadiendo forms a react

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-      <nav>
-        <Link to="/auth/register">Registrarse</Link>
+      <h1 className=" text-4xl font-bold text-white">Inicar sesion</h1>
+      <nav className=" mt-10">
+        <Link
+          className=" text-center text-white text-lg block"
+          to="/auth/register"
+        >
+          Registrarse
+        </Link>
       </nav>
     </>
   );
