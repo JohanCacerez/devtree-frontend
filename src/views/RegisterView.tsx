7b62f6e1f8400f6f74b5ef780f49a23091ecf2a2
JohanCacerez <johancacerez10@gmail.com>
2025-08-02 20:23:11 -0600

    añadiendo toast a mensajes para el usuario

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import ErrorsMessage from "../components/ErrorsMessage";
 import axios, {isAxiosError} from "axios";
+import {toast} from 'sonner'
 
 import type { RegisterForm } from '../types'
 
@@ -26,11 +27,11 @@ export default function RegisterView() {
   const handleRegister = async (formData : RegisterForm) => {
     try {
       const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, formData);
-      console.log(data)
+      toast.success(data)
       reset(); // Reset form after successful registration
     } catch (error) {
       if(isAxiosError(error) && error.response) {
-        console.log(error.response?.data.error);
+        toast.error(error.response?.data.error);
       }
     }
   };
