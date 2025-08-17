516f1d294e9d7c20addd23522889451527b06b86
JohanCacerez <johancacerez10@gmail.com>
2025-08-16 19:06:27 -0600

    ajuste de componenete

--- src/api/DevTreeAPI.ts
+++ src/api/DevTreeAPI.ts
@@ -1,12 +1,13 @@
 import { isAxiosError } from "axios";
 import api from "../config/axios";
+import type { User } from "../types";
 
 
 //Funcion para obtener el usuario
 export async function getUSer() {
   try {
     // Realiza la solicitud a la API para obtener el usuario
-    const { data } = await api("/user");
+    const { data } = await api<User>("/user");
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
